61c61c7a813654332d4e1cc080fe85e6192037ca
georgecatalin <george.calin@gmail.com>
2023-05-23 13:11:19 +0300

    added new files

---
+++ GUI Development with LVGL/RGB_Sliders_Mix_GUI_Project/Core/Src/my_rgb_mixer.h
@@ -0,0 +1,16 @@
+﻿#ifndef RGB_MIX_H
+
+#include "lvgl/lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void rgb_mixer_create_UI(void);
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif // !RGB_MIX_H
